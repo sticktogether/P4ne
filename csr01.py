5fe45f44d7bd5ae6ce49ced4b2ed484c29c14b0d
ex merc <ex@merc.com>
2023-03-21 17:25:10 +0300

    another commit4

--- csr01.py
+++ csr01.py
@@ -1,2 +1,8 @@
 
 print("My first script.\nPowered by CSR. 2023")
+
+def ip (ip="127.0.0.1",mask="/24"):
+    return "Your adress is: "+ip+mask+";"
+r1 = ip(mask="/27")
+r2 = ip("1.1.1.10","/32")
+print(r1+"\n"+r2)

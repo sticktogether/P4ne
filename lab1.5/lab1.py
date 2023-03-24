84ce3ad7e0d4a26aa20ba546875d9d5ceaf4cc93
ex merc <ex@merc.com>
2023-03-24 18:52:02 +0300

    commit14

--- lab1.5/lab1.py
+++ lab1.5/lab1.py
@@ -10,6 +10,6 @@ for filename in glob.glob(os.path.join(path, '*.log')):
                 if len(pars_line) == 4:
                     parsed.append(line[:-1])
                     #print(line)
-#
+
 parsed2 = set(parsed)
 print(parsed2)

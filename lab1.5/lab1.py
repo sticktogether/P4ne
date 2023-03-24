c100d9fea31cc6dea65be8d65a138078a72aa446
ex merc <ex@merc.com>
2023-03-24 17:55:50 +0300

    commit14

--- lab1.5/lab1.py
+++ lab1.5/lab1.py
@@ -1,9 +1,15 @@
 import os, glob
 
 path = 'C:\\Users\ii.azorkin\Downloads\config_files'
+parsed = []
 for filename in glob.glob(os.path.join(path, '*.log')):
     with open(filename) as f:
         for line in f:
             if "ip address" in line:
-                print(line)
-
+                pars_line = line.split()
+                if len(pars_line) == 4:
+                    parsed.append(line[:-1])
+                    #print(line)
+#
+parsed2 = set(parsed)
+print(parsed2)
